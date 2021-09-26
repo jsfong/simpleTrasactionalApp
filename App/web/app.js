e093a8a25d404111b2fa1ff7d373af0b273d0a4d
jsfong <jsfong5105@gmail.com>
2021-09-26 13:38:58 +0800

    Update ELK & Test

--- App/web/app.js
+++ App/web/app.js
@@ -24,17 +24,29 @@ redisClient.on("connect", () => {
 });
 
 
-//Handle rest
-app.get('/', (req, res) => {
+//POST - Perform transaction
+app.post('/', (req, res) => {
     let timeLog = 'Time taken';
     console.time(timeLog);
 
     //Save data to redis
+    let i = 0;
     const keyName = Math.random().toString().substr(2, 10);
-    redisClient.set("test_key", "test_value", redis.print);
+
+    redisClient.set(keyName, "test_value", redis.print);
 
     console.timeEnd(timeLog);
-    res.status(200).send('Hello World');
+    res.status(200).send(`Transaction ID: ${keyName}`);
+});
+
+
+//DELETE - Delete all redis key
+app.delete('/rediskeys', (req, res) => {
+    redisClient.flushall((err, reply) => {
+        if (err) console.error(err);
+        if (reply) console.log(reply);
+        res.status(200).send();
+    });
 });
 
 //Setup server
